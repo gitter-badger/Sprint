c7f6163a37ad913db4b41cc17eb92e6a70997160
Alex Bates <16batesa@gmail.com>
2015-06-02 21:26:40 +0100

    Update editor.js

--- public-html/js/editor.js
+++ public-html/js/editor.js
@@ -3,3 +3,13 @@ editor.getSession().setUseWrapMode(true);
 editor.setShowPrintMargin(false);
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
+
+// set default project
+var defaultCode = 'this.init = function() {\n    this.goTo(0, 0); // same as this.x = 0; this.y = 0;\n    this.costume("Cat1");\n    this.penDown();\n};\n\nthis.update = function() {\n    this.forward(1);\n    this.rotate(3);\n    this.say("I am at: (" + this.x + ", " + this.y + ")");\n};';
+editor.getSession().setValue(defaultCode);
+
+// run the code
+$('#editor-go').on('click', function() {
+	var code = editor.getSession().getValue();
+	eval(code);
+});
